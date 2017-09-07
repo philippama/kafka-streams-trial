0ddcf07d947d7b56c3513dc5c6d741773ee88776
Philippa Main <philippa.main@autotrader.co.uk>
2017-09-07 07:03:40 +0100

    Deserialise from, and serialise to, JSON.

--- src/main/java/pm/kafkastreams/streamtotable/KStreamToGlobalKTable.java
+++ src/main/java/pm/kafkastreams/streamtotable/KStreamToGlobalKTable.java
@@ -1,7 +1,13 @@
 package pm.kafkastreams.streamtotable;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
@@ -12,27 +18,36 @@ import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
 public class KStreamToGlobalKTable {
+
+    private static final String STREAM_INPUT_TOPIC = "streams-join-stream-input";
+    private static final String TABLE_INPUT_TOPIC = "streams-join-table-input";
+    private static final String OUTPUT_TOPIC = "streams-join-output";
+    private static final String STORE_NAME = "table-store";
+
     public static void main(String[] args) throws Exception {
+
+        final Serde<JsonNode> jsonSerde = createJsonNodeSerde();
+
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-join-output");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-join");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+//        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+//        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, jsonSerde.getClass().getName());
 
         // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
         // Note: To re-run the demo, you need to use the offset reset tool:
         // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); //TODO: how can I make this work?
+//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // TODO: how can I make this work?
 
         KStreamBuilder builder = new KStreamBuilder();
 
-        KStream<String, String> source = builder.stream("streams-join-stream-input");
+        KStream<String, JsonNode> source = builder.stream(Serdes.String(), jsonSerde, STREAM_INPUT_TOPIC);
 
         source
-                .peek((key, value) -> printKeyValue(key, value, "Input")) // Added this line to log input to console.
+                .peek((key, value) -> printKeyValue(key, getValueAsString(value), "Input")) // Added this line to log input to console.
 //                .peek((key, value) -> printKeyValue(key, value.toString(), "Output")) // Added these two lines to log results to console.
-                .to("streams-join-output");
+                .to(Serdes.String(), jsonSerde, "streams-join-output");
 
         final KafkaStreams streams = new KafkaStreams(builder, props);
         final CountDownLatch latch = new CountDownLatch(1);
@@ -54,6 +69,24 @@ public class KStreamToGlobalKTable {
             System.exit(1);
         }
         System.exit(0);
+    }
+
+    private static Serde<JsonNode> createJsonNodeSerde() {
+        final Serializer<JsonNode> jsonSerialiser = new JsonSerializer();
+        final Deserializer<JsonNode> jsonDeserialiser = new JsonDeserializer();
+        final Serde<JsonNode> jsonSerde = Serdes.serdeFrom(jsonSerialiser, jsonDeserialiser);
+
+        // Test
+        String json = "{\"time\":\"0722\",\"station\":\"Crewe\"}";
+        JsonNode jsonNode = jsonDeserialiser.deserialize("topic", json.getBytes());
+        ((ObjectNode) jsonNode).put("extra", "hi");
+        System.out.println("Test: " + String.format("[time=%s, station=%s, extra=%s]", jsonNode.get("time").asText(), jsonNode.get("station").asText(), jsonNode.get("extra").asText()));
+        System.out.println("Test: " + getValueAsString(jsonNode));
+        return jsonSerde;
+    }
+
+    private static String getValueAsString(JsonNode jsonNode) {
+        return String.format("[time=%s, station=%s]", jsonNode.get("time").asText(), jsonNode.get("station").asText());
     }
 
     private static void printKeyValue(String key, String value, String label) {
